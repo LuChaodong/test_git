a795290907695ebfcb152ef0f7bded2a239fa429
Orient Lu <lcdsdream@gmail.com>
2014-11-18 19:53:47 +0800

    Update hell.c
    i++

--- hell.c
+++ hell.c
@@ -7,10 +7,10 @@ int main( int argc , char *argv[] )
   int i ;
   printf("Hello github\n");
   printf("SECOND");
-  
+ 
  for( i=0; i<10; i++)
- 	;
-  
+ 	i++;
+
 	printf("THIRD");
 return 0;    
 }
